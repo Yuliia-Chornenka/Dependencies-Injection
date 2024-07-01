f39a146e9a57856ef2e5b2843da1ea042b4208f5
ychornenka <yuliia.chornenka.consultant@springernature.com>
2024-07-01 23:12:51 +0300

    DI added

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,7 @@
+import { Logger } from "../services/logger";
+import { Users } from "../services/users";
+import { HTTP } from "../services/http";
+
 export interface User {
   id: number;
   name: string;
@@ -6,4 +10,11 @@ export interface User {
 export interface ApiConfig {
   path: string;
   resources: { [key: string]: string };
+}
+
+export type IoCResources = {
+  apiConfig: ApiConfig;
+  logger: typeof Logger;
+  users: typeof Users;
+  http: typeof HTTP;
 }
